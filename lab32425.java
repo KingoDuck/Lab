26a3dc7145dceb56bf1bb885c395bfdfee829ec9
KingoDuck <161373206+KingoDuck@users.noreply.github.com>
2025-03-25 11:45:08 -0600

    Add files via upload

---
+++ lab32425.java
@@ -0,0 +1,21 @@
+
+
+public class lab32425 {
+	//Global veriable. Exists everywere
+	int varName = 20;
+	double pi = 3.14
+
+	public static void main(String[] args) {
+		System.out.println(pi);
+		//will print 3.14
+
+
+	}
+	public static void changePi(){
+		//Change pi to 100
+		pi = 100;
+		//
+
+
+	}
+}
